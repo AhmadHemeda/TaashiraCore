779e1e453699ff65da356cd8197538630b7b8359
AhmadHemeda <77977659+AhmadHemeda@users.noreply.github.com>
2024-05-14 11:59:38 +0300

    added TaashiraUtility as dependency

--- Package.swift
+++ Package.swift
@@ -5,17 +5,23 @@ import PackageDescription
 
 let package = Package(
     name: "TaashiraCore",
+    platforms: [.iOS(.v14)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "TaashiraCore",
             targets: ["TaashiraCore"]),
     ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        .package(url: "https://github.com/AhmadHemeda/TaashiraUtility.git", from: "1.0.0"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "TaashiraCore"),
+            name: "TaashiraCore",
+            dependencies: ["TaashiraUtility"]),
         .testTarget(
             name: "TaashiraCoreTests",
             dependencies: ["TaashiraCore"]),
